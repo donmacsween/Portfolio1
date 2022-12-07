617bb66ad005892d7ec293065893730085624352
Don MacSween <donmacsween@gmail.com>
2022-12-07 08:57:31 +0000

    Started Scriptable objects

---
+++ Assets/_Portfolio1/Scripts/ScriptableObjectTemplates/EyeColors.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EyeColours", menuName = "objects/EyeColour")]
+public class EyeColors : ScriptableObject
+{
+    public Color[] colors;
+}
---
+++ Assets/_Portfolio1/Scripts/ScriptableObjectTemplates/SkinColours.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="SkinColours",menuName ="objects/SkinColour")]
+public class SkinColours : ScriptableObject
+
+{
+    public Color[] colors;
+}
